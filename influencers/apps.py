3b71d6a8c914b13052eb23e750b6ca8a0b72cc11
josueadimado <josueadimado@gmail.com>
2026-01-01 23:46:07 +0000

    Fix: Update Django to 5.1.15 for Python 3.10 compatibility

--- influencers/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class InfluencersConfig(AppConfig):
-    name = 'influencers'
--- pushit/settings/prod.py
+++
@@ -1,7 +0,0 @@
-from .base import *
-
-DEBUG = False
-
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",") if os.getenv("DJANGO_ALLOWED_HOSTS") else []
-
-
